8db66d9a582b0a3d7bf037923cc11e6c630fa474
immoo <v5.mo@icloud.com>
2023-06-06 15:00:32 +0800

    初始化

--- OpKanban/OpKanban/Data/Login.cs
+++ OpKanban/OpKanban/Data/Login.cs
@@ -12,17 +12,18 @@ namespace OpKanban.Data
         public string Username { get; set; }
         public Realms.Sync.App app { get; set; }
         public Realms.Sync.User user { get; set; }
-        public Realms.Sync.SyncConfiguration config { get; set; }
+        public Realms.Sync.SyncConfigurationBase config { get; set; }
         public Realm realm { get; set; }
         public string partition { get; set; }
         public event Action OnChange;
         public bool IsLoggedIn { get; set; } = false;
-        public bool LoginFailed {get;set;} = false;
-        public string Gravatar {get;set;}
+        public bool LoginFailed { get; set; } = false;
+        public string Gravatar { get; set; }
 
         public async Task DoLogin(string email, string password)
         {
-            try {
+            try
+            {
                 Username = email;
 
                 using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
@@ -38,15 +39,17 @@ namespace OpKanban.Data
                     }
                     Gravatar = sb.ToString().ToLower();
                 }
-    
+
                 app = Realms.Sync.App.Create("opkanban-lymqs");
                 user = await app.LogInAsync(Realms.Sync.Credentials.EmailPassword(email, password));
-                partition = $"user={ user.Id }";
-                config = new Realms.Sync.SyncConfiguration(partition, user);
+                partition = $"user={user.Id}";
+                config = new Realms.Sync.PartitionSyncConfiguration(partition, user);
                 realm = await Realm.GetInstanceAsync(config);
                 IsLoggedIn = true;
                 LoginFailed = false;
-            } catch (Exception e) {
+            }
+            catch (Exception e)
+            {
                 IsLoggedIn = false;
                 LoginFailed = true;
             }
